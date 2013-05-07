d9f90560d2d051538790f67450158b1231934b8f
adamtw1 <adamtw1@gmail.com>
2013-05-07 01:35:28 -0500

    Finished Main Map Functionality Pending Database
    I have now completed the functionality of the map that can be accessed
    from the toolbar. There is still work to do on the map that is supposed
    to allow a user to select a location.
    VERY IMPORTANT: Leave the target API on Google API, preferably API 17.

--- Plant_Track/src/edu/drake/pocketbotanist/Location_2_3.java
+++ Plant_Track/src/edu/drake/pocketbotanist/Location_2_3.java
@@ -4,34 +4,55 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap.OnMarkerClickListener;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 
-public class Location_2_3 extends Activity {
+public class Location_2_3<GoogleMap> extends Activity implements OnMarkerClickListener {
 
+	@SuppressWarnings("null")
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_location_2_3);
+		setContentView(R.layout.activity_entrymap_1_3);
+	 
+		final LatLng center = new LatLng(42, -93.61);
+		
+		com.google.android.gms.maps.GoogleMap mMap = null;
+		mMap.setOnMarkerClickListener(this);
+		mMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+		CameraPosition cameraPosition = new CameraPosition.Builder()
+			.target(center)
+			.zoom(10)
+			.build();
+		mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+		mMap.setMyLocationEnabled(true);
+		
+		/*public void onMapLongClick(LatLng point) {
+			mMap.addMarker(new MarkerOptions()
+			.position(point)
+			.title("Selected Point")
+			.icon(BitmapDescriptorFactory.fromResource(R.drawable.pb_marker)));
+		}
+		*/
+		
 	}
-
+	
+	
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.location_2_3, menu);
+		getMenuInflater().inflate(R.menu.entrymap_1_3, menu);
 		return true;
 	}
-	
-	
-	
-	//code to change the default marker
-	/*
-	static final LatLng MELBOURNE = new LatLng(-37.81319, 144.96298);
-	Marker melbourne = mMap.addMarker(new MarkerOptions()
-	                          .position(MELBOURNE)
-	                          .title("Melbourne")
-	                          .snippet("Population: 4,137,400")
-	                          .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
-	                          */
-	
-	//GoogleMap.setOnMarkerClickListener(OnMarkerClickListener)
-	//public abstract boolean onMarkerClick (Marker marker)
+
+	@Override
+	public boolean onMarkerClick(Marker marker) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 }
