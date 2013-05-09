2a6b28037db1541c8225c5513ea1ceca618f6bd2
adamtw1 <adamtw1@gmail.com>
2013-05-08 20:57:49 -0500

    User can now drop a pin!
    It doesn't save, but the user can now drop a pin. Going to wait for
    database before I add saving functions.

--- Plant_Track/src/edu/drake/pocketbotanist/Location_2_3.java
+++ Plant_Track/src/edu/drake/pocketbotanist/Location_2_3.java
@@ -3,26 +3,27 @@ package edu.drake.pocketbotanist;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap.OnMarkerClickListener;
+import com.google.android.gms.maps.GoogleMap.OnMapClickListener;
 import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class Location_2_3<GoogleMap> extends Activity implements OnMarkerClickListener {
+public class Location_2_3<GoogleMap> extends Activity implements OnMapClickListener{
 
-	@SuppressWarnings("null")
+	com.google.android.gms.maps.GoogleMap mMap;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_entrymap_1_3);
+		setContentView(R.layout.activity_location_2_3);
 	 
 		final LatLng center = new LatLng(42, -93.61);
 		
-		com.google.android.gms.maps.GoogleMap mMap = null;
-		mMap.setOnMarkerClickListener(this);
 		mMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
 		CameraPosition cameraPosition = new CameraPosition.Builder()
 			.target(center)
@@ -30,29 +31,26 @@ public class Location_2_3<GoogleMap> extends Activity implements OnMarkerClickLi
 			.build();
 		mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
 		mMap.setMyLocationEnabled(true);
-		
-		/*public void onMapLongClick(LatLng point) {
-			mMap.addMarker(new MarkerOptions()
-			.position(point)
-			.title("Selected Point")
-			.icon(BitmapDescriptorFactory.fromResource(R.drawable.pb_marker)));
-		}
-		*/
-		
+		mMap.setOnMapClickListener(this);
 	}
 	
-	
+	public void onMapClick(LatLng point) {
+
+        mMap.animateCamera(CameraUpdateFactory.newLatLng(point));
+                
+        ((com.google.android.gms.maps.GoogleMap) mMap).addMarker(new MarkerOptions()
+        .position(point)
+        .title("New Point")
+        .snippet("Species Name., May 7, 2013 1:24 AM " + point) 
+		.icon(BitmapDescriptorFactory.fromResource(R.drawable.pb_marker))
+		);
+
+    }
 	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.entrymap_1_3, menu);
 		return true;
-	}
-
-	@Override
-	public boolean onMarkerClick(Marker marker) {
-		// TODO Auto-generated method stub
-		return false;
 	}
 }
